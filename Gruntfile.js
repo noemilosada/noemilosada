32853ff7d88bc3252b48a122d7d998dcbbdf696e
Noemi Losada <noemi@alexandalexa.com>
2014-07-02 17:45:08 +0100

    US2 Add less and uglify task to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,33 @@ module.exports = function(grunt) {
     /* JSHINT - syntax checking
     ------------------------------------------ */
     jshint: {
-      all: ['Gruntfile.js', 'js/**/*.js' ]
+      all: ['Gruntfile.js', 'assets/js/**/*.js' ]
+    },
+
+    /* LESS - files from less to css
+    ------------------------------------------ */
+    less: {
+      production: {
+        options: {
+          cleancss: true
+        },
+        files: {
+          "assets/css/theme.css": "assets/less/theme.less"
+        } 
+      }
+    },
+
+    /* UGLIFY - minify js files
+    ------------------------------------------ */
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'assets/js/theme.min.js': ['assets/js/theme.js']
+        }
+      }
     }
 
   });
@@ -19,11 +45,13 @@ module.exports = function(grunt) {
    * Load plugin tasks
    */
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   /**
    * Register tasks
    */
-  grunt.registerTask('build', ['jshint']);
+  grunt.registerTask('build', ['jshint', 'less', 'uglify']);
 
 // END Grunt module
 };
