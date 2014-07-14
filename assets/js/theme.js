925381824fe2e12e49c7bcba6444a03dda76e5ac
Noemi Losada <tbcoel@gmail.com>
2014-07-14 02:29:36 +0100

    US8 Smooth scroll

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,6 +1,26 @@
 /**
- * Test
+ * Name: Theme.js
+ * Date: 14/07/2014
+ * @Author: Noemi Losada <info@noemilosada.com>
  */
-var test = function test() {
-  console.log('this is a test');
-};
+(function($) {
+
+    /** Smooth Scroll */
+    $('.scrollTop a').each(function() {
+        $(this).on("click", function(e) {
+            e.preventDefault();
+
+            var href = $(this).attr("href");
+
+            $('html, body').animate({
+                scrollTop: $(href).offset().top - 60
+            }, 500);
+        });
+    });
+
+    /** Collapse nav on element clicked */
+    $("#main-navigation li a").on("click", function(event) {
+        $(".navbar-collapse").collapse('hide');
+    });
+
+})(jQuery);
